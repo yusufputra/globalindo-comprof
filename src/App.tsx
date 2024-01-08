3843ff61a23af025cac110ce57b935ccd3937a28
Yusuf Putra <yusufputra99@gmail.com>
2024-01-08 08:34:26 +0700

    feat: commitment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Commitment } from './components/section/Commitment';
 import { CompanyWorked } from './components/section/CompanyWorked';
 import { Hero } from './components/section/Hero';
 
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Hero />
       <CompanyWorked />
+      <Commitment />
     </div>
   );
 }
---
+++ src/components/section/Commitment.tsx
@@ -0,0 +1,43 @@
+import { LayoutContainer } from '../LayoutContainer'
+import { Box, Heading, SimpleGrid, Text } from '@chakra-ui/react'
+
+export const Commitment = () => {
+    return (
+        <LayoutContainer py={20} gap={20}>
+            <SimpleGrid columns={2} width='full' display={{ base: 'flex', md: 'grid' }} justifyContent='space-between' flexWrap={{ base: 'wrap', md: 'unset' }} rowGap={10}>
+                <SimpleGrid columns={2} textAlign='left' gap={7}>
+                    <Box>
+                        <Heading as='h1' fontSize='6xl' fontWeight='bold' lineHeight='1'>
+                            245%
+                        </Heading>
+                        <Text color='black.600' fontSize={'md'}>More revenues for the brand</Text>
+                    </Box>
+                    <Box>
+                        <Heading as='h1' fontSize='6xl' fontWeight='bold' lineHeight='1'>
+                            130K+
+                        </Heading>
+                        <Text color='black.600' fontSize={'md'}>Audiences reached</Text>
+                    </Box>
+                    <Box>
+                        <Heading as='h1' fontSize='6xl' fontWeight='bold' lineHeight='1'>
+                            50+
+                        </Heading>
+                        <Text color='black.600' fontSize={'md'}>brands trust us</Text>
+                    </Box>
+                    <Box>
+                        <Heading as='h1' fontSize='6xl' fontWeight='bold' lineHeight='1'>
+                            24+
+                        </Heading>
+                        <Text color='black.600' fontSize={'md'}>Worldwide awards</Text>
+                    </Box>
+                </SimpleGrid>
+                <Box gap={7} display='flex' flexDir='column' textAlign={'left'}>
+                    <Heading as='h1' fontSize={{ base: '4xl', md: '6xl' }} fontWeight='bold' lineHeight='1'>
+                        commitments
+                    </Heading>
+                    <Text color='black.600' fontSize={'md'}>We are committed to working with you collaboratively to understand your goals and create a strategy that will achieve them.</Text>
+                </Box>
+            </SimpleGrid>
+        </LayoutContainer>
+    )
+}
